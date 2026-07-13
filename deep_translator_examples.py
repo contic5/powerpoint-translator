e28fc83bb4f5aa356b9eb64841992eb0ea8ba3dd
Christopher Joseph Conti <36709920+contic5@users.noreply.github.com>
2026-07-13 13:13:55 -0400

    Initial Commit

---
+++ deep_translator_examples.py
@@ -0,0 +1,4 @@
+from deep_translator import GoogleTranslator
+
+# Use any translator you like, in this example GoogleTranslator
+translated = GoogleTranslator(source='auto', target='es').translate("keep it up, you are awesome")  # output -> Weiter so, du bist großartig
---
+++ powerpoint-translator.py
@@ -0,0 +1,62 @@
+from pptx import Presentation
+from deep_translator import GoogleTranslator
+import time
+import pandas as pd
+
+MAX_SHAPES_TO_TRANSLATE=999
+TARGET_LANGUAGE="Spanish"
+
+def translate_presentation(target_file):
+    if not ".pptx" in target_file:
+        target_file=target_file+".pptx"
+
+    out_file=target_file.replace(".pptx",f"_{TARGET_LANGUAGE}.pptx")
+    if(out_file.count("_")==1):
+        out_file=out_file.replace("_"," ")
+    
+    prs = Presentation(f'original/{target_file}')
+    total_shapes=0
+    print(f"Total Slides {len(prs.slides)}")
+    for slide in prs.slides:
+        if total_shapes>=MAX_SHAPES_TO_TRANSLATE:
+            break
+        
+        for shape in slide.shapes:
+            #Try to translate shape text
+            try:
+                translated_text=GoogleTranslator(source='auto', target='es').translate(shape.text)
+                print(translated_text)
+                shape.text=translated_text
+                time.sleep(1)
+                total_shapes+=1
+
+                if total_shapes>=MAX_SHAPES_TO_TRANSLATE:
+                    break
+            #Skip if the shape has no text
+            except AttributeError:
+                pass
+
+    print(f"Total Shapes {total_shapes}")
+    print(f"{target_file} translated to {TARGET_LANGUAGE}")
+    prs.save(f'output/{out_file}')
+
+def get_language_code(TARGET_LANGUAGE):
+    language_iso_df=pd.read_excel("Language_ISO_Codes.xlsx")
+    target_language_row=language_iso_df[language_iso_df["Language"]==TARGET_LANGUAGE]
+    for index,row in target_language_row.iterrows():
+        return row["ISO Code"]
+    return ""
+
+def main():
+    language_code=get_language_code(TARGET_LANGUAGE)
+    print(language_code)
+
+    print("Powerpoint Presentation Translator")
+    print("Put PowerPoint files you want to translate in the 'original' folder")
+    while True:
+        target_file=input("Enter the file name you want to translate. Leave this blank to exit.")
+        target_file=target_file.replace("\n","")
+        translate_presentation(target_file)
+
+if __name__=="__main__":
+    main()
